dcfa1624f748f8ceb5ce7ca1f7c509dcc993a5f5
Hauglid <karltha@ifi.uio.no>
2017-09-12 10:40:00 +0200

    project created

---
+++ ChatbotStarterKit/app/src/main/java/com/computas/kthx/chatbotstarterkit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.computas.kthx.chatbotstarterkit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
